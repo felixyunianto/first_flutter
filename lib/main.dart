9bf8c03ce36b2ac2db0e93aaf499db422d5baadf
felixyunianto <felixyunianto@gmail.com>
2021-06-26 06:04:33 +0700

    2. Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(title: Text("Aplikasi Hello World")),
-          body: Center(child: Text("Hello World"))),
+          body: Center(
+              child: Container(
+                  color: Colors.lightBlue,
+                  width: 150,
+                  height: 100,
+                  child: Text(
+                    "Saya sedang melatih kemampuan flutter saya.",
+                    // maxLines: 2,
+                    overflow: TextOverflow.clip,
+                    // softWrap: true,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20),
+                  )))),
     );
   }
 }
